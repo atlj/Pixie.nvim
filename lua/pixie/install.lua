43adcc73f3483d4e8cba49cdbdcece38970e9b40
Burak <md.burak.guner@gmail.com>
2023-02-02 22:09:46 +0300

    feat: inform the user if PixieInstall fails

--- lua/pixie/install.lua
+++ lua/pixie/install.lua
@@ -6,7 +6,11 @@ local function install()
   -- Just go use Visual Studio Code you are already a Microsoft fan
   -- JK I'm also a Microsoft fan
   local command = "cd " .. project_root .. "/js && " .. "npm install --only=prod && npm run build"
-  os.execute(command)
+  local status = os.execute(command)
+
+  if (status ~= 0) then
+    print("There was an issue during Pixie installation. Please run :PixieInstall manually. If the issue persists, please check the issues https://github.com/atlj/Pixie.nvim/issues")
+  end
 end
 
 return install
