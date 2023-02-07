db87aaf1f8d8a1d8abc927582f596547d8fbc407
Burak Güner <md.burak.guner@gmail.com>
2023-02-07 23:40:35 +0300

    feat: use fn.jobstart instead of os.execute (#2)

--- lua/pixie/run_js.lua
+++ lua/pixie/run_js.lua
@@ -1,9 +1,13 @@
 local project_root = require("pixie.utils").project_root_path
 
 ---@param args string
-local function run_js(args)
+---@param on_success function
+local function run_js(args, on_success)
   local command = "cd " .. project_root .. "/js/lib && " .. "node index.js " .. args
-  os.execute(command)
+  vim.fn.jobstart(command, {
+      -- TODO: check if there are any errors
+      on_exit = on_success
+  })
 end
 
 return run_js
